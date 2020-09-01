4d47340ebc0b93e5e4734b84d38208d0f7bb3878
LoraLJX <422872768@qq.com>
2020-09-01 19:07:16 +0800

    更新2

---
+++ alg-back/src/views/mainpage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <mynav></mynav>
+    <offside class="main"></offside>
+  </div>
+</template>
+<script >
+import offside from "../views/offside";
+import mynav from "../views/Nav";
+export default {
+  // 注册子组件
+  components: {
+    offside,
+    mynav,
+  },
+};
+</script>
+<style scoped>
+.main {
+  background-color:#fff;
+  height:695px ;
+  width: 100%;
+  top: 0px;
+  position: relative;
+  box-sizing: border-box;
+  overflow: hidden;
+}
+</style>
